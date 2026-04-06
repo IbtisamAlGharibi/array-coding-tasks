fb9be71b48cd44c80614bdd1f2854c8633819040
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-06 13:33:41 +0400

    Task 5: no23

--- src/Task5.java
+++ src/Task5.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Task5 {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2};
+
+        if ((nums[0] != 2 || nums[0] != 3) && (nums[1] != 2 || nums[1] != 3)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+    }
 }
