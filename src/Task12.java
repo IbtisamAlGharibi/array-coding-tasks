c55f849755051becca98dbb4c9f383ca66615b74
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-06 13:53:08 +0400

    Task 12: makeEnds

---
+++ src/Task12.java
@@ -0,0 +1,10 @@
+public class Task12 {
+    public static void main(String[] args){
+        int[] nums = {1, 2, 3};
+        int first = nums[0];
+        int last = nums[nums.length -1];
+
+        int[] nums2 = {first, last };
+        System.out.println(nums2);
+    }
+}
