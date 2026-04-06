56a5e0b85ccd903b1d275ff8b7acd89ce0842f82
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-06 13:34:37 +0400

    Task 6: fix23

--- src/Task6.java
+++ src/Task6.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Task6 {
+    public static void main(String[] args){
+
+        int[] nums = {1, 2, 3};
+
+        for (int i=0; i < nums.length -1; i++) {
+            if (nums[i] == 2 && nums[i + 1] == 3)
+                        nums[0] = 0;
+                        nums[1] = 0;
+                        nums[2] = 0;
+                        System.out.println(nums);
+
+        }
+    }
 }
