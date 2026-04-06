3e5376a474d7991f59d5d3d3279952f4aecd42e7
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-06 13:36:54 +0400

    Task 10: sameFirstLast

--- src/Task10.java
+++ src/Task10.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Task10 {
+    public static void main(String[] args){
+
+        int[] nums = {1, 2, 3};
+        if (nums.length > 1 && nums[0] == nums[nums.length]){
+            System.out.println("True");
+        }else{
+            System.out.println("False");
+
+        }
+    }
 }
