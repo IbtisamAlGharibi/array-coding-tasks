b0718981086cd07c5d450b21e7fb06e1231f4f7a
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-06 13:32:06 +0400

    Task 4: middleWay

---
+++ src/Task4.java
@@ -0,0 +1,13 @@
+public class Task4 {
+    public static void main(String[] args){
+        int[] a = {1,2,3};
+        int[] b = {1,2,3};
+
+        int[] nums = new int[];
+
+         nums[0] = a[1];
+         nums[1] = b[1];
+
+        System.out.println(nums);
+    }
+}
