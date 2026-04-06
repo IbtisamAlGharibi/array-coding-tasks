9bd14f2f4118185c61c20c163964b83eb0dace16
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-06 11:03:46 +0400

    Task 3: reverse3

---
+++ src/Task3.java
@@ -0,0 +1,14 @@
+public class Task3 {
+    public static void main (String[] args) {
+
+        int[] nums = {1, 2, 3};
+        int[] rev = new int[3];
+
+        int rev[ 0] =int nums[ 2];
+        int rev[ 1] =int nums[ 1];
+        int rev[ 2] =int nums[ 0];
+
+        System.out.println(rev);
+
+    }
+}
