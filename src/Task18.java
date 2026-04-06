1005965e0853249a01d8f48f0bd7d9134d86b343
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-06 14:59:24 +0400

    Task 18: makePi

---
+++ src/Task18.java
@@ -0,0 +1,11 @@
+public class Task18 {
+    public static void main(String[] args){
+        int [] array = {3, 1, 4};
+
+        for (int i =0; i < array.length-1; i++){
+            System.out.println(array[i]);
+
+        }
+
+    }
+}
