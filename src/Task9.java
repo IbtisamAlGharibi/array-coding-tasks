67b7774f6d6c70798dd7f26e548cbd75e7ca0edb
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-06 13:36:21 +0400

    Task 9: unlucky1

--- src/Task9.java
+++ src/Task9.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Task9 {
+    public static void main(String[] args){
+
+        int[] nums = {1, 3, 4, 5};
+        boolean unlucky = false;
+
+        for (int i = 0; i < nums.length - 1; i++){
+            if (nums[i] == 1 && nums[i + 1] == 3){
+                unlucky = true;
+            }
+        }
+
+        if (unlucky){
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
 }
