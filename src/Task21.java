097ee50ff0b3e36508a09789a5f1729ea2112b0e
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-06 15:20:22 +0400

    Task 21: has23

---
+++ src/Task21.java
@@ -0,0 +1,21 @@
+public class Task21 {
+    public static void main(String[] args){
+
+        int [] array = {2, 5};
+
+        for (int i=0; i< array.length-1; i++){
+
+            if (array[i]==2 || array[1]==3){
+                System.out.println("true");
+
+            }else {
+                System.out.println("false");
+            }
+
+        }
+
+
+
+    }
+
+}
