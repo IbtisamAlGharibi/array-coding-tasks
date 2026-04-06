14b3ebd8f446731dad9d5f4c75b07f494f68d935
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-06 10:36:31 +0400

    Task 1: firstLast6

---
+++ src/JavaBasics.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class JavaBasics {
+    public static void main(String[] args){
+    /*      Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter your name");
+        String name = scan.nextLine();
+
+        System.out.println("Please enter your major");
+        String major = scan.nextLine();
+
+        System.out.println("name is:" +" "+  name + " "+ "major is:"+" " +major);
+ */
+
+        int a = 5;
+        int b = 9;
+        int result = a +b;
+        System.out.println(result);
+    }
+}
---
+++ src/Task1.java
@@ -0,0 +1,14 @@
+public class Task1 {
+    public static void mian(String[] args){
+        int[] nums = {1,2,3};
+
+        if (nums[0]==6 || nums[nums.length-1] == 6){
+
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+
+        }
+
+    }
+}
